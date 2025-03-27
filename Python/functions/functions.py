fa603e31fa5244119bed899fcee228d1728f5fbd
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-27 17:41:14 +0300

    added a calculator function

---
+++ Python/functions/functions.py
@@ -0,0 +1,98 @@
+
+#FUNCTIONS
+#A function is a block of code which only runs when it is called
+
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Emil", "Refsnes") 
+
+#Arguments also known as args are values passed when function is called
+#A variable is inside the function definition
+
+# def greet(name):
+#   print("Hello", name)
+#   print("How do you do")
+  
+# greet("Jack")
+
+#Nested functions:
+#This is a function defined within the body of another function, allowing it to access and modify variables from the outer function scope.
+#this is a nested function example
+# def calculator(operation):
+#     def add(a ,b):
+#         return a + b
+    
+#     def subtract(a ,b):
+#         return a - b
+    
+#     def multiply(a ,b):
+#         return a * b
+    
+#     def divide(a ,b):
+#         return a / b
+    
+#     if operation == "add":
+#         return add
+    
+#     elif operation == "subtract":
+#         return subtract
+    
+#     elif operation == "multiply":
+#         return multiply
+    
+#     elif operation == "divide":
+#         return divide
+#     else:
+#         return"Invalid operation"
+    
+    
+# result_add = calculator("add")(3,4)
+# print(f"addition results:{result_add}")
+
+
+#You can add an operator to a function so the user can enter two numbers and perform arithmetic operations
+#USE CASE:
+def calculator(operaton):
+    def add(a, b):
+        return a + b
+    def subtract(a, b):
+        return a - b
+    def multiply(a, b):
+        return a * b
+    def divide(a, b):
+        return a / b
+    
+    if operation == "add":
+        return add
+    
+    elif operation == "subtract":
+        return subtract
+    
+    elif operation == "multiply":
+        return multiply
+    
+    elif operation == "divide":
+        return divide
+    else:
+        return"Invalid operation"
+    
+    
+# # User input
+operation = input("Enter operation (add, subtract, multiply, divide): ").strip().lower()
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# # Perform calculation
+operation_func = calculator(operation)
+if operation_func:
+    result = operation_func(a, b)
+    print(f"Result: {result}")
+else:
+    print("Invalid operation")
+    
+    
+    
+ 
+  
