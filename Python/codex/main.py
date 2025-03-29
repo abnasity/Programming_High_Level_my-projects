13de9047ba35a72d195afa1addbd55723ba48f11
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-29 22:35:29 +0300

    initial commit

---
+++ Python/codex/main.py
@@ -0,0 +1,63 @@
+
+#Holy moly, because the class's average was super low on the test, the teacher just added a curve for the test grades! 😭
+
+#Create a grades.py program that checks whether a grade is above or below 55.
+
+#Start by creating a variable called grade and give it a value between 0-100.
+
+#Write a if/else statement for the following:
+
+  #  If grade is greater than or equal to 55, then print "You passed."
+   # Else, print "You failed."
+
+#After you run the code, change grade's value and rerun it. Do this a few times to make sure it's working as intended.
+
+
+# grade = 40
+# if grade >= 55:
+#      print("You passed")
+# else:
+#      print("You failed")
+     
+#Relational Operators
+
+#A lot of the times inside conditions, we are comparing two values. To do so, we need to use a different type of operators called relational operators that compares values:
+
+   # == equal to
+  #  != not equal to
+  #  > greater than
+  #  < less than
+  #  >= greater than or equal to
+  #  <= less than or equal to
+
+# Elif
+
+#One or more elif statements (short for "else if") can be optionally added in between the if and else to provide additional condition(s) to check. Sometimes two is simply not enough.
+
+#USE CASES:
+
+#In chemistry, pH is a scale used to specify the acidity or basicity of a liquid. 🧪
+
+#Create a ph_levels.py program that checks whether a pH level is basic, acidic, or neutral.
+
+#First, create a ph variable and ask the user for a value between 0 and 14.
+
+#Write an if, elif, else statement that:
+
+  #  If ph is greater than 7, output "Basic".
+   # If ph is less than 7, output "Acidic".
+    #Else, output "Neutral".
+
+
+ph = int(input("What is the pH level: "))
+
+if ph < 0 or ph > 14:
+    print("Invalid! Please enter a value between 0 and 14.")
+elif ph > 7:
+    print("Basic")
+elif ph < 7:
+    print("Acidic")
+else:  # ph == 7
+    print("Neutral")
+
+        
