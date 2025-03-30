808132fecdfe46a4637844c8a205b9ed9030d840
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-30 22:03:56 +0300

    updated the recursion file

---
+++ Python/recursion/jgjg.py
@@ -0,0 +1,20 @@
+
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+# result = fact(5)
+# print(result)
+
+
+
+
+def fact(n):
+  if n==0:
+    return 1
+  else:
+    return  n * fact(n-1)
+result = fact(9)
+print(result)
