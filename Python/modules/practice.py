d2ec288e2b99e294607a5c3eb41396b9180c6f26
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-31 16:28:23 +0300

    modules and factorial practice

---
+++ Python/modules/practice.py
@@ -0,0 +1,32 @@
+
+
+# ph = int(input("What is the current ph: "))
+# if ph > 14 or ph < 0:
+#   print("Invalid! enter ph of 0-14")
+# elif ph < 7:
+#     print("the ph is acidic")
+# elif ph > 7:
+#     print("the ph is basic")
+# else:
+#     print("Neutral")
+    
+#Create  a faulty calculator 
+
+num1 = float(input("Enter the first number: "))
+operator = input("Enter operator i.e (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+if operator == "+":
+    print(num1 * num2)
+elif operator == "-":
+    print(num1 + num2)
+elif operator == "*":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 * num2)
+
+
+
+
+
+    
+    
