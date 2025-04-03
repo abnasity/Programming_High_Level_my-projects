fb559d02e35ba7af48038f55f0d90bc89e5e8c61
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-03 12:26:35 +0300

    made changes to poly file

---
+++ Python/polymorphism/main.py
@@ -0,0 +1,25 @@
+#Types of polymorphism
+#1)Dynamic polymorphism
+#2)Operator overloading
+#3)Method Overriding
+#4)Method overloading
+
+
+#Dynamic polymorphism
+
+class Duck:
+    def sound(self):
+        print("Quack!")
+
+class Cat:
+    def sound(self):
+        print("Meow!")
+
+def make_sound(animal):  # No need to check type
+    animal.sound()
+
+duck = Duck()
+cat = Cat()
+
+make_sound(duck)  # ✅ Output: Quack!
+make_sound(cat)   # ✅ Output: Meow!
---
+++ Python/polymorphism/practice.py
@@ -0,0 +1,18 @@
+
+class Duck:
+    def sound(self):
+        print("quacks")
+    
+class Cat:
+    def sound(self):
+        print("meows")
+    
+def make_sound(animal):
+        animal.sound()
+        
+duck = Duck()
+cat = Cat()
+
+make_sound(duck)
+make_sound(cat)
+
