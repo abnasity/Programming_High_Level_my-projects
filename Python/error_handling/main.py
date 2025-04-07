9505dc31141f86bb764dd7fe108a0943696e40fc
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-07 16:18:28 +0300

    made some changes to the file

--- Python/error_handling/main.py
+++ Python/error_handling/main.py
@@ -8,11 +8,31 @@
 #     print("sorry something went wrong")
     
     
-try:
-    f = open("test_file.txt")
+# try:
+#     f = open("test_file.txt")
   
-    if f.name == "currupt_file.txt":
-        raise Exception
+#     if f.name == "currupt_file.txt":
+#         raise Exception
     
-except Exception as e:
-    print("Error!")
+# except Exception as e:
+#     print("Error!")
+
+
+# a = 5
+# b = 0
+# try:
+#     print(a/b)
+# except Exception:
+#     print("Hey! you cannot divide a number by zero")
+    
+# try:
+#   print(x)
+# except:
+#   print("An exception occurred")
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong") 
