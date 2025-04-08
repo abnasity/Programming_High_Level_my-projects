ae9e4aefe0eac8e836c110dde23405748c9d2318
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-08 19:03:49 +0300

    primary commit of the file

---
+++ Python/Data_types/main.py
@@ -0,0 +1,12 @@
+
+#***Characterestics of Sequence Objects/Data TYpes
+#1. Ordered - This means that the elements have a defined order and you can access them using an index
+#2. Indexed - This means that you can access the elements using an index
+#3. Mutable - This means that you can change the elements of the sequence
+#4. Allow Duplicates - This means that you can have multiple elements with the same value
+#5. Iterable - This means that you can repeat a process or a set of steps foreach element in the sequence
+#6. Can Be Nested - This means that you can have a sequence inside a sequence
+#7. Can be sliced - This means that you can access a rane of elements in the sequence
+#8. Can be concatenated - This means that you can join two or more sequences together
+#9. Can be reapeted - This means that you can repeat the elements in the sequence
+#10.Can be converted to other data types - This ,eans that you can convert the sequence to other data types i.e set, Dictionaries etc
