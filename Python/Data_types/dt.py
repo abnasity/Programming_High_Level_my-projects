61cc9d9cffd03ef33f5cdc4f0e014d7750621011
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-09 16:19:15 +0300

    modified datatypes/dt.py

---
+++ Python/Data_types/dt.py
@@ -0,0 +1,13 @@
+
+#This is how data types are represented in python:
+#Lists - []
+# Dictionaries - {}
+# Sets - {}
+# Tuples - ()
+# Strings - ''
+# Integers - 0
+# Floats - 0.0
+# Booleans - True/False
+# None - None
+
+
