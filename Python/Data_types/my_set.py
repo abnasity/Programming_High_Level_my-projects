23b5e0ac67e0f53c95f8691f53bd992b265d7ed8
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-31 22:07:24 +0300

    another example

--- Python/Data_types/my_set.py
+++ Python/Data_types/my_set.py
@@ -7,3 +7,5 @@ Teachers = {'Mufti', 'Rop', 'Daisy'}
 students = {'Calvin', 'John', 'Jane', 'Doe', 'Alice'}
 
 products = {'Laptop', 'Phone', 'Tablet', 'Smart Watch', 'Smart TV'}
+
+imei = {'356789123456789', '356789123456788', '356789123456787', '356789123456786', '356789123456785'}
