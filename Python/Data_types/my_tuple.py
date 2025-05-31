82ae20f6eb661d053488c3688e4d2334dcc925b1
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-31 21:15:38 +0300

    another example

--- Python/Data_types/my_tuple.py
+++ Python/Data_types/my_tuple.py
@@ -2,3 +2,4 @@
 # Examples of Tuples in python
 Phones = ('Samsung', 'Iphone', 'Infinix', 'Tecno', 'X-tigi', 'Nokia')
 Tea_Factories = ('Kapset', 'Chemalal', 'Stegro', 'Kapkoros', 'Motigo')
+imei = ('356789123456789', '356789123456788', '356789123456787', '356789123456786', '356789123456785')
