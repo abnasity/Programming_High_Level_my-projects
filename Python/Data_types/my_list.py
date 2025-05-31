a42605948b32f23052491a259d66ed3c4cd84dbf
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-31 23:46:25 +0300

    another example

--- Python/Data_types/my_list.py
+++ Python/Data_types/my_list.py
@@ -2,3 +2,4 @@
 oranges = ['banana', 'apple', 'bitruit']
 vehicles = ['car', 'bus', 'motorcycle', 'bicycle']
 cars = ['Toyota', 'Honda', 'Ford', 'Chevrolet']
+clubs = ['Manchester United', 'Chelsea', 'Arsenal', 'Liverpool']
