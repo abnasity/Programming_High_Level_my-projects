899e9ff266247c79e68e822225bbb30b9d2e5c86
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-31 23:55:43 +0300

    another example

--- Python/Data_types/my_list.py
+++ Python/Data_types/my_list.py
@@ -4,3 +4,6 @@ vehicles = ['car', 'bus', 'motorcycle', 'bicycle']
 cars = ['Toyota', 'Honda', 'Ford', 'Chevrolet']
 clubs = ['Manchester United', 'Chelsea', 'Arsenal', 'Liverpool']
 laptops = ['Dell', 'HP', 'Lenovo', 'Apple']
+
+#lists with mixed data types
+mixed_list = ['John', 25, 5.9, True, None, ['nested', 'list'], {'key': 'value'}]
