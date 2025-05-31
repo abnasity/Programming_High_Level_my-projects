5d8ceb6f0672b7ac3f33e3b02727b1c15eba3709
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-31 22:01:12 +0300

    another example

--- Python/Data_types/my_set.py
+++ Python/Data_types/my_set.py
@@ -5,3 +5,5 @@ books = {'Encyclopaedia', 'Social studies', 'Maths'}
 Teachers = {'Mufti', 'Rop', 'Daisy'}
 
 students = {'Calvin', 'John', 'Jane', 'Doe', 'Alice'}
+
+products = {'Laptop', 'Phone', 'Tablet', 'Smart Watch', 'Smart TV'}
