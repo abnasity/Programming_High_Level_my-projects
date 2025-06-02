635f7ba633511dd12342c19b58f16f103a2b1b0a
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-02 19:16:03 +0300

    Creating an item

--- Python/Data_types/my_set.py
+++ Python/Data_types/my_set.py
@@ -9,3 +9,9 @@ students = {'Calvin', 'John', 'Jane', 'Doe', 'Alice'}
 products = {'Laptop', 'Phone', 'Tablet', 'Smart Watch', 'Smart TV'}
 
 imei = {'356789123456789', '356789123456788', '356789123456787', '356789123456786', '356789123456785'}
+
+
+# Creating a set
+fruits = {"apple", "banana", "cherry", "apple"}
+
+print(fruits)  # Output: {'banana', 'cherry', 'apple'} (no duplicates)
