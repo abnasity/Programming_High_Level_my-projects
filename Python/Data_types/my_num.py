57fa9cc7f1a99d0f10a94671f2e95850f2c3f1cd
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-02 19:02:34 +0300

    basic arithmetic

--- Python/Data_types/my_num.py
+++ Python/Data_types/my_num.py
@@ -8,3 +8,9 @@ num = 50.7
 nums = 50.9 + 50.8
 total = num + nums
 print(f"sum {total}")
+
+# Basic Arithmetic
+a = 5 + 3      # 8
+b = 10 - 4     # 6
+c = 4 * 7      # 28
+d = 20 / 4     # 5.0 (returns float)
