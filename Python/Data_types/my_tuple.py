4e287c81823439f69d862fe7d27b7807be3f5cfe
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-02 19:42:32 +0300

    accessing elements

--- Python/Data_types/my_tuple.py
+++ Python/Data_types/my_tuple.py
@@ -5,3 +5,11 @@ Tea_Factories = ('Kapset', 'Chemalal', 'Stegro', 'Kapkoros', 'Motigo')
 imei = ('356789123456789', '356789123456788', '356789123456787', '356789123456786', '356789123456785')
 products = ('Laptop', 'Phone', 'Tablet', 'Smart Watch', 'Smart TV')
 customers = ('Calvin', 'John', 'Jane', 'Doe', 'Alice')
+
+# Creating tuples
+t = (1, 2, 3)
+t2 = ("a", "b", "c")
+t3 = (1, "hello", True)
+# Accessing elements
+print(t[0])  # Output: 1
+print(t2[1])  # Output: 'b'
